822145b292196c8ef47f98452082f698103dc8a6
Carlos Amorim <carlos.av.amorim@gmail.com>
2021-12-27 22:42:43 -0300

    Table do dashboard finalizado

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Sumary from '../Sumary'
+import { TransactionTable } from '../TransactionTable'
 import { Container } from './styles'
 
 export default function Dashboard() {
     return (
         <Container>
             <Sumary />
-
+            <TransactionTable />
         </Container>
     )
 }
